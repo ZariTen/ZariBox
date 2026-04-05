1f01badbc938237230daaee16486842b4bf79892
ZariTen <zariten0@outlook.com>
2026-04-05 20:25:45 -0300

    feat: add nix flake install method

--- flake.nix
+++ flake.nix
@@ -1,25 +1,26 @@
 {
-  description = "ZariBox nix shell";
+  description = "ZariBox - declarative container manager";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-    in
-    {
+    in {
+      packages.${system}.default = pkgs.python3Packages.buildPythonApplication {
+        pname = "zaribox";
+        version = "0.1.5";
+        src = ./.;
+        format = "pyproject";
+        propagatedBuildInputs = with pkgs.python3Packages; [ pyyaml setuptools ];
+      };
+
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = [
-          (pkgs.python3.withPackages (ps: [
-            ps.pyyaml
-            ps.pytest
-          ]))
+          (pkgs.python3.withPackages (ps: [ ps.pyyaml ps.pytest ]))
           pkgs.git
         ];
-
         shellHook = ''
           echo "ZariBox dev shell"
           echo "Python $(python --version)"
