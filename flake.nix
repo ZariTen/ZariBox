e640fedc9b57bfdc663cd059942890817081f7f4
ZariTen <zariten0@outlook.com>
2026-04-05 20:08:07 -0300

    feat: add initial flake.nix and flake.lock for Nix shell configuration

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "ZariBox nix shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          (pkgs.python3.withPackages (ps: [
+            ps.pyyaml
+          ]))
+          pkgs.git
+        ];
+
+        shellHook = ''
+          echo "ZariBox dev shell"
+          echo "Python $(python --version)"
+        '';
+      };
+    };
+}
