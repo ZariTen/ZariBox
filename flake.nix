8fa917f53809eaf40fc044172b90b0909198d1cb
ZariTen <zariten0@outlook.com>
2026-04-08 08:25:13 -0300

    chore: update package version and enhance Nix flake

--- flake.nix
+++ flake.nix
@@ -5,31 +5,51 @@
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in {
-      packages.${system}.default = pkgs.python3Packages.buildPythonApplication {
-        pname = "zaribox";
-        version = "0.1.6.1";
-        src = ./.;
-        format = "pyproject";
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.python3Packages.buildPythonApplication {
+            pname = "zaribox";
+            version = "0.1.6.2";
+            src = ./.;
+            format = "pyproject";
 
-        nativeBuildInputs = with pkgs.python3Packages; [
-          setuptools
-        ];
+            nativeBuildInputs = with pkgs.python3Packages; [ 
+              setuptools 
+            ];
+            
+            propagatedBuildInputs = with pkgs.python3Packages; [ 
+              pyyaml 
+            ];
 
-        propagatedBuildInputs = with pkgs.python3Packages; [ pyyaml ];
-      };
+            nativeCheckInputs = with pkgs.python3Packages; [ 
+              pytestCheckHook 
+            ];
+          };
+        });
 
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          (pkgs.python3.withPackages (ps: [ ps.pyyaml ps.pytest ps.flake8 ]))
-          pkgs.git
-        ];
-        shellHook = ''
-          echo "ZariBox dev shell"
-          echo "Python $(python --version)"
-        '';
-      };
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.default ];
+            
+            packages = [
+              pkgs.python3Packages.flake8
+              pkgs.git
+            ];
+
+            shellHook = ''
+              echo "ZariBox dev shell"
+              echo "Python $(python --version)"
+            '';
+          };
+        });
     };
 }
