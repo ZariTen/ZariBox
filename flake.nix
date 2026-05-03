f61a52b957e87118e8220bc6849400067769f2ea
ZariTen <zariten0@outlook.com>
2026-05-03 12:47:58 -0300

    flake: pin src to GitHub release tag instead of local path

--- flake.nix
+++ flake.nix
@@ -1,13 +1,12 @@
 {
   description = "ZariBox - declarative container manager";
-
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-
   outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      version = "0.1.7";
     in {
       packages = forAllSystems (system:
         let
@@ -15,20 +14,22 @@
         in {
           default = pkgs.python3Packages.buildPythonApplication {
             pname = "zaribox";
-            version = "0.1.7";
-            src = ./.;
+            inherit version;
+            src = pkgs.fetchFromGitHub {
+              owner = "ZariTen";
+              repo = "zaribox";
+              rev = "v${version}";
+              hash = "sha256-ihylz1Wjk4Vqg6NNEdVPBhKX6ZMqvWxpMZ1U+7n9K04=";
+            };
             format = "pyproject";
-
-            nativeBuildInputs = with pkgs.python3Packages; [ 
-              setuptools 
+            nativeBuildInputs = with pkgs.python3Packages; [
+              setuptools
             ];
-            
-            propagatedBuildInputs = with pkgs.python3Packages; [ 
-              pyyaml 
+            propagatedBuildInputs = with pkgs.python3Packages; [
+              pyyaml
             ];
-
-            nativeCheckInputs = with pkgs.python3Packages; [ 
-              pytestCheckHook 
+            nativeCheckInputs = with pkgs.python3Packages; [
+              pytestCheckHook
             ];
           };
         });
@@ -39,12 +40,10 @@
         in {
           default = pkgs.mkShell {
             inputsFrom = [ self.packages.${system}.default ];
-            
             packages = [
               pkgs.python3Packages.flake8
               pkgs.git
             ];
-
             shellHook = ''
               echo "ZariBox dev shell"
               echo "Python $(python --version)"
