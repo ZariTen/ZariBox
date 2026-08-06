2ff153622997e9e10c93a3bf472069873d5a6e6d
ZariTen <zariten0@outlook.com>
2026-08-06 10:57:50 -0300

    chore(flake): modernize outputs and set up nixfmt formatter

--- flake.nix
+++ flake.nix
@@ -1,54 +1,77 @@
 {
   description = "ZariBox - declarative container manager";
+
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
-  outputs = { self, nixpkgs }:
+
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      forAllSystems =
+        f: nixpkgs.lib.genAttrs supportedSystems (system: f nixpkgs.legacyPackages.${system});
 
       version = "0.2.5";
-    in {
-      packages = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in {
-          default = pkgs.python3Packages.buildPythonApplication {
-            pname = "zaribox";
-            inherit version;
-            src = pkgs.fetchFromGitHub {
-              owner = "ZariTen";
-              repo = "zaribox";
-              rev = "v${version}";
-              hash = "sha256-+9XL2NG+OFFIp+x4WtP3J2ouJyuGK6M25IMyvz/M+lQ=";
-            };
-            format = "pyproject";
-            nativeBuildInputs = with pkgs.python3Packages; [
-              setuptools
-            ];
-            propagatedBuildInputs = with pkgs.python3Packages; [
-              pyyaml
-            ];
-            nativeCheckInputs = with pkgs.python3Packages; [
-              pytestCheckHook
-            ];
-          };
-        });
+    in
+    {
+      formatter = forAllSystems (pkgs: pkgs.nixfmt);
 
-      devShells = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in {
-          default = pkgs.mkShell {
-            inputsFrom = [ self.packages.${system}.default ];
-            packages = [
-              pkgs.python3Packages.flake8
-              pkgs.git
-            ];
-            shellHook = ''
-              echo "ZariBox dev shell"
-              echo "Python $(python --version)"
-            '';
+      packages = forAllSystems (pkgs: {
+        default = pkgs.python3Packages.buildPythonApplication {
+          pname = "zaribox";
+          inherit version;
+
+          src = pkgs.fetchFromGitHub {
+            owner = "ZariTen";
+            repo = "zaribox";
+            rev = "v${version}";
+            hash = "sha256-+9XL2NG+OFFIp+x4WtP3J2ouJyuGK6M25IMyvz/M+lQ=";
           };
-        });
+
+          format = "pyproject";
+
+          nativeBuildInputs = with pkgs.python3Packages; [
+            setuptools
+          ];
+
+          propagatedBuildInputs = with pkgs.python3Packages; [
+            pyyaml
+          ];
+
+          nativeCheckInputs = with pkgs.python3Packages; [
+            pytestCheckHook
+          ];
+
+          meta = {
+            description = "Declarative container manager";
+            homepage = "https://github.com/ZariTen/zaribox";
+            mainProgram = "zaribox";
+          };
+        };
+      });
+
+      checks = forAllSystems (pkgs: {
+        zaribox = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+      });
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${pkgs.stdenv.hostPlatform.system}.default ];
+
+          packages = [
+            pkgs.python3Packages.flake8
+            pkgs.git
+            pkgs.nixfmt
+          ];
+
+          shellHook = ''
+            echo "ZariBox dev shell"
+            echo "Python $(python --version)"
+          '';
+        };
+      });
     };
 }
